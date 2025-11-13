c2fa463db8bb51dd87b14a6030972e1f3139f988
rafaelfp1985 <43796907+rafaelfp1985@users.noreply.github.com>
2025-11-13 15:56:35 +0000

    more tests

--- pancake_swap.py
+++ pancake_swap.py
@@ -49,7 +49,9 @@ def get_usda_price():
 
 # Swap function
 def swap_tokens():
+    st.write("Starting wallet...")
     nonce = web3.eth.get_transaction_count(wallet_address)
+    st.write("Start swapping...")
     tx = router_contract.functions.swapExactTokensForTokens(
         amount_in,
         0,
@@ -62,6 +64,7 @@ def swap_tokens():
         'nonce': nonce
     })
 
+    st.write("Getting TX...")
     signed_tx = web3.eth.account.sign_transaction(tx, private_key)
     tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
     return web3.to_hex(tx_hash)
