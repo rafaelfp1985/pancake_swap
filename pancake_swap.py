446bf485e650e71d2fa2b99e3eb2d226f1a527d3
rafaelfp1985 <43796907+rafaelfp1985@users.noreply.github.com>
2025-11-14 21:15:11 +0000

    show also bnb token

--- pancake_swap.py
+++ pancake_swap.py
@@ -147,7 +147,7 @@ amount_out_min = int(amount_in * (1 - slippage_tolerance))
 
 #Checking Tokens balance
 bnb_balance = web3.eth.get_balance(Web3.to_checksum_address(wallet_address))
-print(web3.from_wei(bnb_balance, 'ether'))
+st.write("BNB balance", web3.from_wei(bnb_balance, 'ether'))
 usdt_balance = get_token_balance(usdt_address, router_abi, wallet_address)
 st.write("USDT balance", f"${usdt_balance:.4f}")
 usda_balance = get_token_balance(usda_address, router_abi, wallet_address)
