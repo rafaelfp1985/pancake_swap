7de641e2a2a6ef9fbac1ba9093ab9f12de918b52
rafaelfp1985 <43796907+rafaelfp1985@users.noreply.github.com>
2025-11-14 21:14:29 +0000

    fix ABI

--- pancake_swap.py
+++ pancake_swap.py
@@ -108,8 +108,25 @@ web3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 # Load ABI
 st.write("Loading ABI file...")
-with open('abi.json', 'r') as abi_file:
-    router_abi = json.load(abi_file)
+#with open('abi.json', 'r') as abi_file:
+#    router_abi = json.load(abi_file)
+router_abi = [
+    {
+        "constant": True,
+        "inputs": [{"name": "_owner", "type": "address"}],
+        "name": "balanceOf",
+        "outputs": [{"name": "balance", "type": "uint256"}],
+        "type": "function",
+    },
+    {
+        "constant": True,
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [{"name": "", "type": "uint8"}],
+        "type": "function",
+    },
+]
+
 
 # Contract setup
 st.write("Setting up contracts...")
